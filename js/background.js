49491e91fbc3e85fe106946d0e78215a03a01016
DESKTOP-H95JS0M\timeless <timeless@liongames.co.kr>
2022-09-20 10:24:28 +0900

    css  작업

--- js/background.js
+++ js/background.js
@@ -1,14 +1,19 @@
-const MAX_IMAGELENGTH = 8;
-//const bgImage = document.createElement('img');
-//const imgPath = `img/${Math.floor(Math.random()*MAX_IMAGELENGTH)}.jpg`;
-//bgImage.className = 'background';
-//bgImage.src=imgPath;
-//canvas.appendChild(bgImage);
-
+const MAX_IMAGELENGTH = 3;
+const bgImage = document.createElement('img');
 const imgPath = `img/${Math.floor(Math.random()*MAX_IMAGELENGTH)}.jpg`;
-const canvas = document.body.querySelector('#canvas');
-console.log(canvas);
-canvas.style.border='1px solid gray';
-canvas.style.backgroundImage=`url("${imgPath}")`;
-canvas.style.backgroundSize='cover';
-canvas.style.backgroundRepeat='repeat';
+bgImage.src = imgPath;
+bgImage.id = 'bgImage';
+
+const divElement = document.createElement('div');
+divElement.id = 'backgroundImage';
+divElement.appendChild(bgImage);
+document.body.appendChild(divElement);
+
+
+// const imgPath = `img/${Math.floor(Math.random()*MAX_IMAGELENGTH)}.jpg`;
+// const canvas = document.body.querySelector('#canvas');
+// console.log(canvas);
+// canvas.style.border='1px solid gray';
+// canvas.style.backgroundImage=`url("${imgPath}")`;
+// canvas.style.backgroundSize='cover';
+// canvas.style.backgroundRepeat='repeat';
--- js/clock.js
+++ js/clock.js
@@ -3,9 +3,10 @@ const clock = document.querySelector("h2#clock");
 function getClock() {
     const date = new Date();
 
+    const hour = `${date.getHours()}`.padStart(2, 0);
     const min = `${date.getMinutes()}`.padStart(2, 0);
     const sec = `${date.getSeconds()}`.padStart(2, 0);
-    clock.innerText = `${min}:${sec}`;
+    clock.innerText = `${hour}:${min}:${sec}`;
 }
 
 
